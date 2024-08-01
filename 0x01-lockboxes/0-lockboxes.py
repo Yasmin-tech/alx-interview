4ae586daad66caff4f5cd85d974590f6e9d843c3
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-08-01 18:06:54 +0300

    another code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,26 +3,16 @@
 '''
 
 
-def join(T, R):
-    res = []
-    for e in R:
-        res += T[e]
-        return res
-
-
 def canUnlockAll(boxes):
-    '''Checks if all the boxes in a list of boxes containing the keys
-    (indices) to other boxes can be unlocked given that the first
-    box is unlocked.
-    '''
-    index = 0
-    total = list(set(boxes[0]) | {0})
-    added = True
-    while added:
-        added = False
-        for j in join(boxes, total[index:]):
-            if j not in total:
-                total.append(j)
-                index += 1
-                added = True
-    return len(total) == len(boxes)
+    opened = set()
+    queue = [0]
+
+    while queue:
+        box = queue.pop(0)
+        if box not in opened:
+            opened.add(box)
+            for key in boxes[box]:
+                if key < len(boxes):
+                    queue.append(key)
+
+    return len(opened) == len(boxes)
