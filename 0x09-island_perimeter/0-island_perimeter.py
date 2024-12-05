ca83b68845bd1b01ad6d79d25021e0bcfbc1de76
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-12-05 23:33:17 +0300

    summit task 0

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Island perimeter.
+"""
+
+
+def island_perimeter(grid):
+    """Computes the perimeter of an island.
+    doesn’t have “lakes”
+        (water inside that isn’t connected to the water surrounding
+        the island).
+    """
+    perimeter = 0
+    if type(grid) is not list:
+        return 0
+    n = len(grid)
+    for i, row in enumerate(grid):
+        m = len(row)
+        for j, cell in enumerate(row):
+            if cell == 0:
+                continue
+            edges = (
+                i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
+                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
+                i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
+                j == 0 or row[j - 1] == 0,
+            )
+            perimeter += sum(edges)
+    return perimeter
